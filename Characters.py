301c3474d81986c8cf842e643d6e8bd7466645ae
Chris Nielsen <chris2nielsen@gmail.com>
2024-03-21 23:07:29 -0700

    Renamed non-wasd controlled sprites to teammates and wasd controlled to player, centered movements for teammates.

--- Characters.py
+++ Characters.py
@@ -2,48 +2,47 @@
 import pygame
 
 class Character:
-    def __init__(self, name, x, y, width, height, color, speed, isActive):
+    def __init__(self, name, xPos, yPos, width, height, color, speed, isActive):
         self.name = name
-        self.x = x
-        self.y = y
+        self.xPos = xPos
+        self.yPos = yPos
         self.width = width
         self.height = height
         self.color = color
         self.speed = speed
-        self.targetX = x
-        self.targetY = y
+        self.targetX = xPos
+        self.targetY = yPos
         self.isActive = isActive
 
     def draw(self, window):
-        pygame.draw.rect(window, self.color, (self.x, self.y, self.width, self.height))
+        pygame.draw.rect(window, self.color, (self.xPos, self.yPos, self.width, self.height))
 
     def move_towards_target(self):
-        if self.targetX > self.x:
-            self.x += min(self.speed, self.targetX - self.x)
-        elif self.targetX < self.x:
-            self.x -= min(self.speed, self.x - self.targetX)
+        if self.targetX > self.xPos:
+            self.xPos += min(self.speed, self.targetX - self.xPos)
+        elif self.targetX < self.xPos:
+            self.xPos -= min(self.speed, self.xPos - self.targetX)
 
-        if self.targetY > self.y:
-            self.y += min(self.speed, self.targetY - self.y)
-        elif self.targetY < self.y:
-            self.y -= min(self.speed, self.y - self.targetY)
+        if self.targetY > self.yPos:
+            self.yPos += min(self.speed, self.targetY - self.yPos)
+        elif self.targetY < self.yPos:
+            self.yPos -= min(self.speed, self.yPos - self.targetY)
 
     def move_WASD(self, directions, screen_width, screen_height):
         for direction in directions:
             if direction == 'up':
-                new_y = max(self.height // 2, self.y - self.speed)
+                new_y = max(0, self.yPos - self.speed)
             elif direction == 'down':
-                new_y = min(screen_height - self.height // 2, self.y + self.speed)
+                new_y = min(screen_height - self.height, self.yPos + self.speed)
             else:
-                new_y = self.y
-
+                new_y = self.yPos
             if direction == 'left':
-                new_x = max(self.width // 2, self.x - self.speed)
+                new_x = max(0, self.xPos - self.speed)
             elif direction == 'right':
-                new_x = min(screen_width - self.width // 2, self.x + self.speed)
+                new_x = min(screen_width - self.width, self.xPos + self.speed)
             else:
-                new_x = self.x
+                new_x = self.xPos
 
-            self.x, self.y = new_x, new_y
+            self.xPos, self.yPos = new_x, new_y
 
 
